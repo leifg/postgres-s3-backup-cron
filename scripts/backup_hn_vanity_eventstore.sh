1bd87c7b4567eb7986a1338e5bf6c4b442b0d050
Leif Gensert <leif@leif.io>
2018-01-06 22:45:57 +0700

    use dedicated user for hn_vanity_backups

--- scripts/backup_hn_vanity_eventstore.sh
+++ scripts/backup_hn_vanity_eventstore.sh
@@ -3,8 +3,8 @@
 /usr/local/bin/entrypoint.sh backup \
   --postgres-host ${POSTGRES_HOST} \
   --postgres-port ${POSTGRES_PORT} \
-  --postgres-user ${POSTGRES_USER} \
-  --postgres-password ${POSTGRES_PASSWORD} \
+  --postgres-user ${HN_VANITY_POSTGRES_USER} \
+  --postgres-password ${HN_VANITY_POSTGRES_PASSWORD} \
   --aws-access-key-id ${AWS_ACCESS_KEY_ID} \
   --aws-secret-access-key ${AWS_SECRET_ACCESS_KEY} \
   --postgres-db hn_vanity_eventstore \
