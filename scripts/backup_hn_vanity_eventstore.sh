fb3c525f6452db844d486c286f0b00f94e50adde
Leif Gensert <leif@leif.io>
2018-01-06 23:47:17 +0700

    use global bucket

--- scripts/backup_hn_vanity_eventstore.sh
+++ scripts/backup_hn_vanity_eventstore.sh
@@ -8,4 +8,4 @@
   --aws-access-key-id ${AWS_ACCESS_KEY_ID} \
   --aws-secret-access-key ${AWS_SECRET_ACCESS_KEY} \
   --postgres-db hn_vanity_eventstore \
-  --aws-s3-bucket hn-vanity-backups
+  --aws-s3-bucket ${AWS_BACKUP_BUCKET}
